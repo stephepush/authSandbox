96a7ebefaad2b092e4dcf96cd6ccf782323aa2cc
Stephen <7speter@gmail.com>
2021-10-01 14:27:09 -0400

    Standard middleware chain

--- middleware/app.js
+++ middleware/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('<h1>Hello World</h1>')
-});
+function middleware1(req, res, next) {
+    console.log('I am a middleware');
+    next();
+}
+
+function standardExpressCallback(requestObject, responseObject, nextMiddleware) {
+    console.log('I am the standard Express function')
+    responseObject.send('<h1>Hello World</h1>')
+}
+
+app.get('/', middleware1, standardExpressCallback);
 
 app.listen(3000);
