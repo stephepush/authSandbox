bb7cda19c925d77b5aae426bb388b641c05679cf
Stephen <7speter@gmail.com>
2021-10-01 15:21:24 -0400

    This time two app.use invoked middlewares

--- middleware/app.js
+++ middleware/app.js
@@ -3,13 +3,17 @@ const express = require('express');
 const app = express();
 
 app.use(middleware1) /*middleware1 becomes a middleware of the global scope*/
-
+app.use(middleware2)
 
 function middleware1(req, res, next) {
     console.log('I am middleware1');
     next();
 }
 
+function middleware2(req, res, next) {
+    console.log('I am middleware2');
+    next();
+}
 
 
 function standardExpressCallback(requestObject, responseObject, nextMiddleware) {
