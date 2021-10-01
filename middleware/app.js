fdf50e81610d0dc90e7802c20fa5e9d6ba941b61
Stephen <7speter@gmail.com>
2021-10-01 15:18:14 -0400

    Middleware invoked in app.use method vs. middleware involked in app.get

--- middleware/app.js
+++ middleware/app.js
@@ -2,16 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(middleware1) /*middleware1 becomes a middleware of the global scope*/
+
+
 function middleware1(req, res, next) {
-    console.log('I am a middleware');
+    console.log('I am middleware1');
     next();
 }
+
+
 
 function standardExpressCallback(requestObject, responseObject, nextMiddleware) {
     console.log('I am the standard Express function')
     responseObject.send('<h1>Hello World</h1>')
 }
 
-app.get('/', middleware1, standardExpressCallback);
+app.get('/', standardExpressCallback);
 
 app.listen(3000);
