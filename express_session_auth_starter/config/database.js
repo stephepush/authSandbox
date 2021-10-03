1c7de2a1c76e14d86ad7019543a114d725a59869
Stephen <7speter@gmail.com>
2021-10-02 22:32:24 -0400

    Adds user class and adjusts module.exports in config/database

--- express_session_auth_starter/config/database.js
+++ express_session_auth_starter/config/database.js
@@ -1,12 +1,24 @@
 const mysql2 = require('mysql2');
 
-const options = {
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: 'hello',
-    database: 'es_starter'
+const pool = {
+        host: 'localhost',
+        port: 3306,
+        user: 'admin',
+        password: 'hello',
+        database: 'es_starter'
+    } //maybe should be const options
+
+class User {
+    constructor(user_id, username, hash, salt) {
+        this.user_id = user_id;
+        this.username = username;
+        this.hash = hash;
+        this.salt = salt;
+    }
 }
 
 
-module.exports = pool.promise();
+module.exports = {
+    promisePool: pool.promise(),
+    User: User
+};
