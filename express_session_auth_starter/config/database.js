79cf81ca2cd74cde694b9df9d001c519cad8ea3c
Stephen <7speter@gmail.com>
2021-10-02 22:12:30 -0400

    Initial mysql2 code in config/database.js

---
+++ express_session_auth_starter/config/database.js
@@ -0,0 +1,12 @@
+const mysql2 = require('mysql2');
+
+const options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'admin',
+    password: 'hello',
+    database: 'es_starter'
+}
+
+
+module.exports = pool.promise();
