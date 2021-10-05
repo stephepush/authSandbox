ebeb38324e096c0a098c2621c9d981984c5a3a4f
Stephen <7speter@gmail.com>
2021-10-05 17:21:03 -0400

    Adds passport-local package to get things working

--- express_session_auth_starter/app.js
+++ express_session_auth_starter/app.js
@@ -38,6 +38,8 @@ app.use(session({
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
  */
+require('./config/passport')
+
 app.use(passport.initialize());
 app.use(passport.session())
     /**
