9f19dcdbdb96f3962f44ec1c4b3010be7d19b2d3
Stephen <7speter@gmail.com>
2021-10-05 17:30:17 -0400

    fills out passwordUtil, uncomments passport require in routes file

---
+++ express_session_auth_starter/lib/passwordUtils.js
@@ -0,0 +1,8 @@
+//Todo
+
+function validPassword(password, hash, salt) {}
+
+function genPassword(password) {}
+
+module.exports.validPassword = validPassword;
+module.exports.genPassword = genPassword;
