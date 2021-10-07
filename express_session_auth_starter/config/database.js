c4860a2aec54552c0300b06e90426d5a11653168
Stephen <7speter@gmail.com>
2021-10-06 23:03:21 -0400

    Reformats config/database to make its parts appropriately exportable

--- express_session_auth_starter/config/database.js
+++ express_session_auth_starter/config/database.js
@@ -1,19 +1,21 @@
-const mysql2 = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-const pool = {
-        host: 'localhost',
-        port: 3306,
-        user: 'admin',
-        password: 'hello',
-        database: 'es_starter'
-    } //maybe should be const options
 
-const connection = mysql2.createPool(pool);
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'admin',
+    password: 'hello',
+    database: 'es_starter'
+}); //maybe should be const options
+
+const connection = pool;
+
 
 
 class User {
-    constructor(user_id, username, hash, salt) {
-        this.user_id = user_id;
+    constructor(username, hash, salt) {
+        //this.user_id = user_id;
         this.username = username;
         this.hash = hash;
         this.salt = salt;
@@ -26,14 +28,29 @@ class User {
     };
 
     save() {
-        return connection.execute(
-            "INSERT INTO users (username, hash, salt) VALUES (?, ?, ?)", [this.username, this.hash, this.salt] //do i need to use 'this'?
-        )
+        /*         return connection.execute(
+                    "INSERT INTO users (username, hash, salt) VALUES (?, ?, ?)", [this.username, this.hash, this.salt] //do i need to use 'this'?
+                ) */
+        try {
+            return connection.execute(
+                "INSERT INTO users (username, hash, salt) VALUES (?, ?, ?)", [this.username, this.hash, this.salt] //do i need to use 'this'?,
+            ).catch(e => {
+                console.log('error', e);
+            });
+        } catch (e) {
+            console.log('error', e);
+        }
     }
 }
 
 
-module.exports = {
+/* module.exports = {
     connection: connection,
-    User: User
-};
+    User: User,
+    pool: pool
+}; */
+
+module.exports.connection = connection;
+module.exports.pool = pool;
+/*pool and connection are kinda sorta redundant but why not?*/
+module.exports.User = User;
