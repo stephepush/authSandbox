cdb69c7dc7bae160fa215e323a49588ae94a6174
Stephen <7speter@gmail.com>
2021-10-08 15:27:02 -0400

    Adds some console.logs() to see whats happening in genPassword function in lib/passwordUtils

--- express_session_auth_starter/lib/passwordUtils.js
+++ express_session_auth_starter/lib/passwordUtils.js
@@ -6,6 +6,9 @@ function genPassword(password) {
     let salt = crypto.randomBytes(32).toString('hex'); //seasoning recipe
     let genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
 
+    console.log("genHash = " + genHash)
+    console.log('\n')
+    console.log("salt = " + salt)
 
     return {
         salt: salt,
@@ -15,6 +18,9 @@ function genPassword(password) {
 
 function validatesPassword(password, hash, salt) {
     const hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    console.log(hash)
+    console.log('\n')
+    console.log(hashVerify)
     return hash === hashVerify;
 }
 
