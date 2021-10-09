381f6a4282b0dd69b48a68c4d7ce2fc6fa591e8c
Stephen <7speter@gmail.com>
2021-10-09 17:44:49 -0400

    Creates isAdmin method in authMiddleWare file

--- express_session_auth_starter/routes/authMiddleWare.js
+++ express_session_auth_starter/routes/authMiddleWare.js
@@ -7,5 +7,9 @@ module.exports.isAuth = (req, res, next) => {
 }
 
 module.exports.isAdmin = (req, res, next) => {
-
+    if (req.isAuthenticated() && Boolean(req.user.admin)) {
+        next();
+    } else {
+        res.status(401).json({ msg: "you are not authorized to view this resource because you are not an admin" })
+    }
 }
