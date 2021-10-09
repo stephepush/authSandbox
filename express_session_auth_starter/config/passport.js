09a77fdf5700c36c1d05c412c11c3caa2899b862
Stephen <7speter@gmail.com>
2021-10-08 22:53:03 -0400

    Changes property seriealizeUser looks for on line 53 to user_id

--- express_session_auth_starter/config/passport.js
+++ express_session_auth_starter/config/passport.js
@@ -50,7 +50,7 @@ const strategy = new LocalStrategy(customFields, verifyCallback);
 passport.use(strategy);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.user_id);
 })
 
 passport.deserializeUser((userId, done) => {
