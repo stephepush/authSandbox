fb2672e53b407fa1b73371a4a30c7b0bd3ae90b9
Stephen <7speter@gmail.com>
2021-10-10 15:18:43 -0400

    Adds privateKey and Decrypt functionality to main.js

--- public-key-cryptography/main.js
+++ public-key-cryptography/main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const encrypt = require('./encrypt');
+const decrypt = require('./decrypt');
 
 const publicKey = fs.readFileSync(__dirname + '/id_rsa_pub.pem', 'utf8');
 
@@ -8,3 +9,9 @@ const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'Super secret m
 
 //Cracking the code is just gibberish
 console.log(encryptedMessage.toString());
+
+const privateKey = fs.readFileSync(__dirname + 'id_rsa_priv.pem', 'utf8');
+
+const decryptedMessage = decrypt.decryptWithPrivateKey(privateKey, encryptedMessage);
+
+console.log(decryptedMessage.toString());
