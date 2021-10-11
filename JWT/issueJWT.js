8667d14b9ca6435cb6ca9e2b4c1906090784115f
Stephen <7speter@gmail.com>
2021-10-10 22:55:56 -0400

    Adds code to encode jsons

--- JWT/issueJWT.js
+++ JWT/issueJWT.js
@@ -1,6 +1,27 @@
-const base64url = require('base64url');
+const base64 = require('base64url');
 
-const JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ';
+const headerObj = {
+    alg: 'RS256',
+    type: 'JWT'
+}
+
+const payloadObj = {
+    sub: '1234567890',
+    name: 'John Doe',
+    admin: true,
+    iat: 1516239022
+}
+
+const headerObjString = JSON.stringify(headerObj);
+const payloadObjString = JSON.stringify(payloadObj);
+
+const base64UrlHeader = base64(headerObjString);
+const base64UrlPayload = base64(payloadObjString);
+
+console.log(base64UrlHeader);
+console.log(base64UrlPayload)
+
+/* const JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ';
 
 const jwtParts = JWT.split('.');
 
@@ -16,4 +37,4 @@ const decodedSignature = base64url.decode(signatureInBase64UrlFormat);
 
 console.log(decodedHeader)
 console.log(decodedPayload)
-console.log(decodedSignature)
+console.log(decodedSignature) */
